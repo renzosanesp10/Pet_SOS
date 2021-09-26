ca1ac6745a1a7e31f7d44402cfb426cc2ae63e3c
diegojva <diego_jesus_dj@hotmail.com>
2021-09-26 13:51:15 -0500

    Inicio del Proyecto

---
+++ pet_sos_api/src/main/java/com/petsos/api/PetSosApiApplication.java
@@ -0,0 +1,13 @@
+package com.petsos.api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PetSosApiApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(PetSosApiApplication.class, args);
+    }
+
+}
---
+++ pet_sos_api/src/test/java/com/petsos/api/PetSosApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.petsos.api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PetSosApiApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
