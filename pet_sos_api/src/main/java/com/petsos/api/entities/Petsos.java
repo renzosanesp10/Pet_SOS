64430a1c9cc81144f407e63aeaa43855c36a3c41
diegojva <diego_jesus_dj@hotmail.com>
2021-10-16 15:53:01 -0500

    (Backend) ImConfiguración de datasource - entities - Petsos

---
+++ pet_sos_api/src/main/java/com/petsos/api/entities/Petsos.java
@@ -0,0 +1,30 @@
+package com.petsos.api.entities;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import javax.persistence.*;
+
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@Entity
+@Table(name = "petsos")
+
+public class Petsos {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(name="name")
+    private String name;
+    @Column(name="description")
+    private String description;
+    @Column(name="image")
+    private String image;
+
+}
