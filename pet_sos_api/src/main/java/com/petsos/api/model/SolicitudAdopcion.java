1f7483e29302925931a4c0f56063f35173acbad6
Leidy Rodríguez Vásquez <LeidyRV>
2021-10-22 22:26:59 -0500

    Implentando Solicitud Adopcion

---
+++ pet_sos_api/src/main/java/com/petsos/api/model/SolicitudAdopcion.java
@@ -0,0 +1,71 @@
+package com.petsos.api.model;
+
+import lombok.Data;
+import javax.persistence.*;
+import java.time.LocalDateTime;
+
+@Data
+@Entity
+@Table(name="solicitud_adopcion")
+
+public class SolicitudAdopcion {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Integer idSolicitud;
+
+    @Column(name = "fecha", nullable = false)
+    private LocalDateTime fechaPublicacion;
+
+    @Column(String="descripcion", nullable = false, length = 250)
+    private String descripcion;
+
+    @ManyToOne
+    @JoinColumn(name = "id_publicacion", nullable = false,
+            foreignKey = @ForeignKey(name = "Fk_solicitudAdopcion_publicacion"))
+    private Publicaciones  publicacion;
+
+    @ManyToOne
+    @JoinColumn(name = "id_adoptante", nullable = false,
+            foreignKey = @ForeignKey(name = "Fk_solicitudAdopcion_adoptante"))
+    private Adoptante adoptante;
+
+    public Integer getIdSolicitud() {
+        return idSolicitud;
+    }
+
+    public void setIdSolicitud(Integer idSolicitud) {
+        this.idSolicitud = idSolicitud;
+    }
+
+    public LocalDateTime getFechaPublicacion() {
+        return fechaPublicacion;
+    }
+
+    public void setFechaPublicacion(LocalDateTime fechaPublicacion) {
+        this.fechaPublicacion = fechaPublicacion;
+    }
+
+    public String getDescripcion() {
+        return descripcion;
+    }
+
+    public void setDescripcion(String descripcion) {
+        this.descripcion = descripcion;
+    }
+
+    public Publicaciones getPublicacion() {
+        return publicacion;
+    }
+
+    public void setPublicacion(Publicaciones publicacion) {
+        this.publicacion = publicacion;
+    }
+
+    public Adoptante getAdoptante() {
+        return adoptante;
+    }
+
+    public void setAdoptante(Adoptante adoptante) {
+        this.adoptante = adoptante;
+    }
+}
