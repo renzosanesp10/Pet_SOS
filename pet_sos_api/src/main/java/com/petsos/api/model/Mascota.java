f9ebaf4dfe84b06631263e8af0526b7feb9a5f37
juliopretell <jpretellp3@upao.edu.pe>
2021-10-23 01:12:32 -0500

    Avance Mascota

--- pet_sos_api/src/main/java/com/petsos/api/model/Mascota.java
+++ pet_sos_api/src/main/java/com/petsos/api/model/Mascota.java
@@ -4,6 +4,7 @@ import lombok.Getter;
 import lombok.Setter;
 
 import javax.persistence.*;
+import javax.validation.constraints.NotNull;
 
 @Entity
 @Table(name = "mascotas")
@@ -13,14 +14,32 @@ public class Mascota {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Integer idMascota;
+    @NotNull
+    @Column (name="tipos", nullable = false, length = 50)
     private String tipo;
+    @NotNull
+    @Column (name="nombres", nullable = false, length = 50)
     private String nombre;
+    @NotNull
+    @Column (name="edades", nullable = false )
     private Integer edad;
-    private String tamaño;
+    @NotNull
+    @Column (name="tamannos", nullable = false, length = 20)
+    private String tamanno;
+    @NotNull
+    @Column (name="sexos", nullable = false, length = 10)
     private String sexo;
+    @NotNull
+    @Column (name="nivelActividades", nullable = false, length = 10)
     private String nivelActividad;
+    @NotNull
+    @Column (name="descripciones", nullable = false, length = 100)
     private String descripcion;
+    @NotNull
+    @Column (name="fotos", nullable = false, length = 100)
     private String foto;
+    @NotNull
+    @Column (name="estados", nullable = false, length = 1)
     private String estado;
 
 }
