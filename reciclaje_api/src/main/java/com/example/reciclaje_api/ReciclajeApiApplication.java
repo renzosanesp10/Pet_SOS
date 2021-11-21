727931f078b42279be9d4b2714d9cc8423845343
diegojva <diego_jesus_dj@hotmail.com>
2021-11-20 23:23:28 -0500

    modificaciones y mejores a las clases

---
+++ reciclaje_api/src/main/java/com/example/reciclaje_api/ReciclajeApiApplication.java
@@ -0,0 +1,13 @@
+package com.example.reciclaje_api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ReciclajeApiApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ReciclajeApiApplication.class, args);
+    }
+
+}
---
+++ reciclaje_api/src/test/java/com/example/reciclaje_api/ReciclajeApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.reciclaje_api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ReciclajeApiApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
